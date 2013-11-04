fa8de623677c4ad5dd33026327eb0c48d8bf7b7b
Darin Glatt <d@mobilerq.com>
2013-11-04 15:38:38 -0800

    Changed SNSRequests methods to public static

--- src/main/java/com/mobilerq/amazonaws/sns/SNSRequests.java
+++ src/main/java/com/mobilerq/amazonaws/sns/SNSRequests.java
@@ -4,31 +4,31 @@ import java.io.InputStream;
 
 public class SNSRequests {
 
-    public CreatePlatformApplicationRequestBuilder createAPNSApp(InputStream p12, char[] p12Password) {
+    public static CreatePlatformApplicationRequestBuilder createAPNSApp(InputStream p12, char[] p12Password) {
         return new CreatePlatformApplicationRequestBuilder(Platform.APNS, p12, p12Password);
     }
 
-    public CreatePlatformApplicationRequestBuilder createAPNSSandboxApp(InputStream p12, char[] p12Password) {
+    public static CreatePlatformApplicationRequestBuilder createAPNSSandboxApp(InputStream p12, char[] p12Password) {
         return new CreatePlatformApplicationRequestBuilder(Platform.APNS_SANDBOX, p12, p12Password);
     }
 
-    public CreatePlatformApplicationRequestBuilder createGCMApp(String name, String apiKey) {
+    public static CreatePlatformApplicationRequestBuilder createGCMApp(String name, String apiKey) {
         return new CreatePlatformApplicationRequestBuilder(Platform.GCM, name, apiKey);
     }
 
-    public CreatePlatformApplicationRequestBuilder createADMApp(String name, String clientSecret) {
+    public static CreatePlatformApplicationRequestBuilder createADMApp(String name, String clientSecret) {
         return new CreatePlatformApplicationRequestBuilder(Platform.ADM, name, clientSecret);
     }
 
-    public CreatePlatformEndpointRequestBuilder createEndpoint(PlatformApplicationArn arn, String token) {
+    public static CreatePlatformEndpointRequestBuilder createEndpoint(PlatformApplicationArn arn, String token) {
         return new CreatePlatformEndpointRequestBuilder(arn, token);
     }
 
-    public PublishRequestBuilder publish(String message) {
+    public static PublishRequestBuilder publish(String message) {
         return new PublishRequestBuilder(message);
     }
 
-    public PublishRequestBuilder publish(PublishMessage message) {
+    public static PublishRequestBuilder publish(PublishMessage message) {
         return new PublishRequestBuilder(message);
     }
 }
